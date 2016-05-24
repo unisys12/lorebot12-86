7d2cbe53d124dfbf0571d5a1a93faad8f39f4ff3
Phillip Jackson <unisys12@gmail.com>
2016-05-24 07:11:03 -0500

    add searchItems and searchCards methods

--- bot.js
+++ bot.js
@@ -11,21 +11,34 @@ function searchGrimoire() {
 
         if(siteSearch) {
             bot.reply(message, "http://www.ishtar-collective.net/search/" + encodeURIComponent(stripeCmd));
-        }
-
+        } return
     });
 }
 
 function searchCard() {
     bot.on("message", function(message) {
         var input = message.content;
-        var stripeCmd = input.substr('7');
+        var stripeCmd = input.substr('6');
         var cardSearch = input.startsWith('!card');
         var trailingUri = stripeCmd.replace(/\s+/g, "-");
 
         if(cardSearch) {
             bot.reply(message, "http://www.ishtar-collective.net/cards/" + trailingUri);
-        }
+        } return
+    });
+}
+
+function searchItems() {
+    bot.on("message", function(message) {
+        var input = message.content;
+        var stripeCmd = input.substr('6');
+        var cardSearch = input.startsWith('!item');
+        var stripApostrophe = stripeCmd.replace("'", "");
+        var trailingUri = stripApostrophe.replace(/\s+/g, "-");
+
+        if(cardSearch) {
+            bot.reply(message, "http://www.ishtar-collective.net/items/" + trailingUri);
+        } return
     });
 }
 
@@ -34,6 +47,8 @@ bot.loginWithToken(process.env.CLIENT_ID, function (token, err) {
         console.log(err);
     }
 
-    searchGrimoire();
-    //searchCard();
+    searchGrimoire();    
+    searchCard();
+    searchItems();
+    
 });
