146236fad7a450720f2ca68f7cb467394c60ffaf
Phillip Jackson <unisys12@gmail.com>
2016-05-24 06:04:13 -0500

    add card method and update some vars

--- bot.js
+++ bot.js
@@ -7,12 +7,25 @@ function searchGrimoire() {
     bot.on("message", function(message) {
         var input = message.content;
         var stripeCmd = input.substr('8');
-        var inputCheck = input.startsWith('!search');
+        var siteSearch = input.startsWith('!search');
 
-        if(inputCheck) {
+        if(siteSearch) {
             bot.reply(message, "http://www.ishtar-collective.net/search/" + encodeURIComponent(stripeCmd));
         }
 
+    });
+}
+
+function searchCard() {
+    bot.on("message", function(message) {
+        var input = message.content;
+        var stripeCmd = input.substr('7');
+        var cardSearch = input.startsWith('!card');
+        var trailingUri = stripeCmd.replace(/\s+/g, "-");
+
+        if(cardSearch) {
+            bot.reply(message, "http://www.ishtar-collective.net/cards/" + trailingUri);
+        }
     });
 }
 
@@ -22,4 +35,5 @@ bot.loginWithToken(process.env.CLIENT_ID, function (token, err) {
     }
 
     searchGrimoire();
+    searchCard();
 });
