bae51d4d719df003133f3fe5cbb137a58de7541e
Phillip Jackson <unisys12@gmail.com>
2016-05-23 23:24:22 -0500

    trying port binding issue workaround

---
+++ server.js
@@ -0,0 +1,2 @@
+//Not actually a server, but using to clear a port binding issue on Heroku
+var http = require('http'); http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
