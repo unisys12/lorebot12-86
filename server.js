cfec1a8dd96ce905b3bc35461c99049a808fab71
Phillip Jackson <unisys12@gmail.com>
2016-06-02 05:47:55 -0500

    add simple node interval to ping site every 5 mins

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 //Not actually a server, but using to clear a port binding issue on Heroku
-var http = require('http'); http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
+var http = require('http'); 
+http.createServer(function (req, res) { 
+    res.writeHead(200, {'Content-Type': 'text/plain'}); 
+    res.send('it is running\n'); })
+.listen(process.env.PORT || 5000);
+
+setInterval(function() {
+    http.get("http://calm-meadow-67261.herokuapp.com");
+}, 300000);
