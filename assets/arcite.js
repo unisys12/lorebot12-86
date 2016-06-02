32edcfbe6300405b233a9df88d0ea71dabbc2605
Phillip Jackson <unisys12@gmail.com>
2016-06-02 06:26:02 -0500

    add assets and cases for more NPC's

---
+++ assets/arcite.js
@@ -0,0 +1,8 @@
+module.exports = [
+"You want the Crucible? Shaxx is the Crucible.",
+"Yee-e-ess?",
+"Get back out there.",
+"Transaction failed. Transaction failed. Transaction complete.",
+"Twilight Gap overbooked, shuttle back or wait.",
+"Cayde wants to talk. Doesn't sound happy.",
+];
