f2a554dd0ea8a3af4988acd9564d310979e27c5d
Phillip Jackson <unisys12@gmail.com>
2016-12-31 13:03:36 -0600

    first round of testing

---
+++ test/HaloTests/halo-tests.js
@@ -0,0 +1,66 @@
+// Testing Dependencies
+const chai = require('chai');
+const expect = chai.expect;
+
+// App Dependencies N/A
+const halo = require('../../scripts/Halo/halo');
+const scripts = require('../../scripts/scripts');
+const spreadsheet = require('../testData/sheetresponse');
+const values = spreadsheet.values;
+
+describe('halo', function() {
+
+  it('getMonthlyActivities() should return an array of filtered events matching the current month of the year', function() {
+    const res = halo.getMonthlyActivities(values);
+
+    expect(res).to.be.a('array');
+    for (var i = 0; i < res.length; i++) {
+      expect(res[i][1]).to.equal(scripts.curMonth());
+    }
+  })
+
+  it('getDailyActivities() should return an array of filtered events from monthlyActivities that match the current day of the week or fire getNonMatchingEvents()', function(){
+    const monthlyEvents = halo.getMonthlyActivities(values);
+    const dailyEvents = halo.getDailyActivities(monthlyEvents);
+    const todayInHalo = [];
+    const nonMatchingEvents = [];
+
+    if (dailyEvents) {
+      expect(nonMatchingEvents).to.be.empty;
+      expect(todayInHalo).to.be.empty;
+
+      for (var i = 0; i < todayInHalo.length; i++) {
+        expect(todayInHalo[i][2]).to.equal(scripts.curDay());
+      }
+    }else{
+      halo.getNonMatchingEvents(values)
+      expect(todayInHalo).to.be.empty;
+    }
+  })
+
+  it('getNonMatchingEvents() should return an array of events containing "N/A" in the month and/or year column', function() {
+    const nonMatchingEvents = [];
+    let results = halo.getNonMatchingEvents(spreadsheet);
+
+    expect(results).to.be.a('array');
+    for (var i = 0; i < results.length; i++) {
+      let row = results[i];
+      expect(row[1] | row[2]).to.equal("N/A");
+    }
+  })
+
+  it('messageConstruct() should contruct a message inside an array from the results of getDailyActivities()', function() {
+    const spreadsheet = require('../testData/sheetresponse');
+    const rows = spreadsheet.values;
+    let message = halo.messageConstruct(rows);
+
+    expect(message).to.be.a('array');
+  })
+
+  // it('haloRequest() should fetch the spreadsheet and fire the messageConstruct() with the rows of the sheet', function() {
+  //   let message = halo.haloRequest();
+  //
+  //   expect(message).to.be.a('array')
+  // })
+
+})
