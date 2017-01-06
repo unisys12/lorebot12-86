4596a78089e41a6d324a94e8ceb519b2ee772d02
Phillip Jackson <unisys12@gmail.com>
2017-01-06 10:44:35 -0600

    get today_in_halo message sent

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ if (!process.env.TOKEN) {
 
 const destiny = require('./scripts/Destiny/destiny');
 const halo = require('./scripts/Halo/halo');
+const scripts = require('./scripts/scripts')
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
@@ -17,14 +18,45 @@ bot.login(process.env.TOKEN, function (token, err) {
 });
 
 bot.on("ready", function() {
-  // Today In Halo (Getting the rest working first)
-  // let channel = bot.channels.get('name', 'lorebot').id;
-  //
-  // if (channel) {
-  //   setInterval(function() {
-  //     return haloRequest(channel)
-  //   }, (1000*60)*24);
-  // }
+  //Today In Halo
+  let channels =  bot.channels;
+  let lb_channel = channels.find('name', 'lorebot')
+  let lb_id = channels.find('name', process.env.halo_channel).id
+
+  if (lb_channel) {
+
+    const google = require('googleapis');
+    const sheet = google.sheets('v4');
+
+    setInterval(function() {
+      sheet.spreadsheets.values.get({
+        key: process.env.googleSheetsKey,
+        spreadsheetId: process.env.googleSheetID,
+        range: process.env.googleSheetRange
+      }, function(err, response) {
+        if (err) {
+          return new Error('Error accessing spreadsheet', err)
+        }else{
+          let rows = response.values;
+          if (rows.length == 0) {
+            return "No rows found! Something happend to the spreadsheet!!"
+          }else{
+            let message = halo.gatherMessage(rows)
+
+              try {
+
+                lb_channel.sendMessage(message)
+
+              } catch (e) {
+
+                throw( new Error('There was an error assembling your message: ' + e) )
+
+              }
+          }
+        }
+      });
+    }, process.env.halo_timer);
+  }
 });
 
 bot.on("message", function (message) {
