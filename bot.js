006ed089cc4f43ac5b30eaa2825c27953a8a9f59
Phillip Jackson <unisys12@gmail.com>
2017-01-08 13:23:14 -0600

    ready event not working, crashing

--- bot.js
+++ bot.js
@@ -18,6 +18,8 @@ bot.login(process.env.TOKEN, function (token, err) {
 });
 
 bot.on("ready", function() {
+  console.log('LoreBot12-86 is ready and running!')
+
   //Today In Halo
   let channels =  bot.channels;
   let lb_channel = channels.find('name', 'lorebot')
@@ -29,38 +31,36 @@ bot.on("ready", function() {
     const sheet = google.sheets('v4');
 
     setInterval(function() {
-      sheet.spreadsheets.values.get({
-        key: process.env.googleSheetsKey,
-        spreadsheetId: process.env.googleSheetID,
-        range: process.env.googleSheetRange
-      }, function(err, response) {
-        if (err) {
-          return new Error('Error accessing spreadsheet', err)
+    sheet.spreadsheets.values.get({
+      key: process.env.googleSheetsKey,
+      spreadsheetId: process.env.googleSheetID,
+      range: process.env.googleSheetRange
+    }, function(err, response) {
+      if (err) {
+        return new Error('Error accessing spreadsheet', err)
+      }else{
+        let rows = response.values;
+        if (rows.length == 0) {
+          return "No rows found! Something happend to the spreadsheet!!"
         }else{
-          let rows = response.values;
-          if (rows.length == 0) {
-            return "No rows found! Something happend to the spreadsheet!!"
-          }else{
-            let message = halo.gatherMessage(rows)
-
-              try {
-
-                lb_channel.sendMessage(message)
-
-              } catch (e) {
-
-                throw( new Error('There was an error assembling your message: ' + e) )
-
-              }
+          console.log('LoreBot12-86 has found a lot of data on the subject...')
+          let message = halo.gatherMessage(rows)
+            try {
+              lb_channel.sendMessage(message)
+            } catch (e) {
+              throw( new Error('There was an error assembling your message: ' + e) )
+            }
           }
         }
-      });
-    }, process.env.halo_timer);
+      })
+    }, process.env.halo_timer)
   }
 });
 
 bot.on("message", function (message) {
     let input = message.content;
+    let channel = message.channel
+    let user = message.author
 
     let quoteCmd = input.startsWith('!quotes');
     let helpCmd = input.startsWith('!lorehelp');
@@ -68,9 +68,17 @@ bot.on("message", function (message) {
     let cardCmd = input.startsWith('!card');
     let siteCmd = input.startsWith('!search');
 
-    if (quoteCmd) { destiny.quotes(input,message) };
-    if (helpCmd) { destiny.help(input, message) };
-    if (itemCmd) { destiny.searchItems(input, message) };
-    if (cardCmd) { destiny.searchCard(input, message) };
-    if (siteCmd) { destiny.searchGrimoire(input, message) };
+    if (quoteCmd) { message.reply(destiny.quotes(input)) };
+    if (helpCmd) { user.sendMessage(destiny.help(input)) };
+    if (itemCmd) { message.reply(destiny.searchItems(input)) };
+    if (cardCmd) { message.reply(destiny.searchCard(input)) };
+    if (siteCmd) { message.reply(destiny.searchGrimoire(input)) };
 });
+
+bot.on('error', function() {
+  console.log(error);
+})
+
+bot.on('disconnect', function() {
+  console.log('LoreBot12-86 has disconnected from Discord Services')
+})
