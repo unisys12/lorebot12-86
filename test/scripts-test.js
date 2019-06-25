fa970a3dcdad8323ae38f85c4774d0b744d46ec7
Phillip Jackson <unisys12@gmail.com>
2019-06-24 19:08:08 -0500

    add prettier formatting and test for esMonths

--- test/scripts-test.js
+++ test/scripts-test.js
@@ -1,34 +1,35 @@
-const chai = require('chai');
+const chai = require("chai");
 const expect = chai.expect;
-const scripts = require('../scripts/scripts');
+const scripts = require("../scripts/scripts");
 
-describe('scripts', function() {
-
-  it('normalizeCardInput() should make all characters lowercase, strip colons and replace spaces with a dash', function(){
+describe("scripts", function() {
+  it("normalizeCardInput() should make all characters lowercase, strip colons and replace spaces with a dash", function() {
     let testString = "Ghost Fragment: Legends";
-    expect(scripts.normalizeCardInput(testString)).to.equal("ghost-fragment-legends");
-  })
+    expect(scripts.normalizeCardInput(testString)).to.equal(
+      "ghost-fragment-legends"
+    );
+  });
 
-  it('normalizeItemInput() should make all characters lowercase, strip semi-colons and replace spaces with a dash', function(){
+  it("normalizeItemInput() should make all characters lowercase, strip semi-colons and replace spaces with a dash", function() {
     let testString = "Saint-14's Ring";
     expect(scripts.normalizeItemInput(testString)).to.equal("saint-14s-ring");
-  })
+  });
 
-  it('randomQuote() should return a random index from an Object', function() {
-    const quotes = require('./TestData/petra');
+  it("randomQuote() should return a random index from an Object", function() {
+    const quotes = require("./TestData/petra");
     const quoteOne = scripts.randomQuote(quotes);
     const quoteTwo = scripts.randomQuote(quotes);
     expect(quoteOne).to.not.equal(quoteTwo);
-  })
+  });
 
-  it('curDay() should return the current day', function() {
+  it("curDay() should return the current day", function() {
     const date = new Date();
     const CurrentDay = date.getDate();
     const day = scripts.curDay();
     expect(day).to.equal(CurrentDay);
-  })
+  });
 
-  it('curMonth() should return the current month as a string', function() {
+  it("curMonth() should return the current month as a string", function() {
     const date = new Date();
     const TestMonth = date.getMonth();
     const months = [
@@ -47,8 +48,52 @@ describe('scripts', function() {
     ];
     const currentTestMonth = months[TestMonth];
     const scriptMonth = scripts.curMonth();
-    expect(currentTestMonth).to.be.a('string');
+    expect(currentTestMonth).to.be.a("string");
     expect(scriptMonth).to.equal(currentTestMonth);
-  })
-
-})
+  }),
+    it("esMonths() should return the in-game version of the current month as a string", function() {
+      const date = new Date();
+      const TestMonth = date.getMonth();
+      switch (TestMonth) {
+        case 0:
+          return "Morning Star";
+          break;
+        case 1:
+          return "Sun's Dawn";
+          break;
+        case 2:
+          return "First Seed";
+          break;
+        case 3:
+          return "Rain's Hand";
+          break;
+        case 4:
+          return "Second Seed";
+          break;
+        case 5:
+          return "Mid Year";
+          break;
+        case 6:
+          return "Sun's Height";
+          break;
+        case 7:
+          return "Last Seed";
+          break;
+        case 8:
+          return "Hearthfire";
+          break;
+        case 9:
+          return "Frostfall";
+          break;
+        case 10:
+          return "Sun's Dusk";
+          break;
+        case 11:
+          return "Evening Star";
+          break;
+        default:
+          "";
+          break;
+      }
+    });
+});
